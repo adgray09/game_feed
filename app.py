fff23c9e6aa021d8dfdfdb8c23895598f43b9f37
adgray09 <alexandergray37@yahoo.com>
2019-10-17 11:10:18 -0700

    added mongo and modeling

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
+
+client = MongoClient()
+db = client.Game_feed
+games = db.games
 
 app = Flask(__name__)
 
-
-
-games = [
-    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
-    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
-]
 @app.route('/')
 def games_index():
     """Show all games."""
-    return render_template('games_index.html', games=games)
+    return render_template('games_index.html', games=games.find())
 
 if __name__ == '__main__':
     app.run(debug=True)
