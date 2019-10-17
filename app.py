d3ded1b1b1c2f8e9b605db8a291297999ee57716
adgray09 <alexandergray37@yahoo.com>
2019-10-17 11:00:51 -0700

    users can now see list of games

--- app.py
+++ app.py
@@ -2,11 +2,16 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
+
+games = [
+    { 'title': 'Cat Videos', 'description': 'Cats acting weird' },
+    { 'title': '80\'s Music', 'description': 'Don\'t stop believing!' }
+]
 @app.route('/')
-def index():
-    """Return homepage."""
-    return render_template('home.html', msg='Flask is Cool!!')
-    return render_template('home.html', msg1='Flask is Amazing!!')
+def games_index():
+    """Show all games."""
+    return render_template('games_index.html', games=games)
 
 if __name__ == '__main__':
     app.run(debug=True)
