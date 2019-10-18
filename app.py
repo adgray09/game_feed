b5611086aaea779e38fffe554983f8c302830375
adgray09 <alexandergray37@yahoo.com>
2019-10-17 18:47:24 -0700

    users can submit

--- app.py
+++ app.py
@@ -1,34 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 client = MongoClient()
-db = client.Game_feed
+db = client.intense
 games = db.games
 
 app = Flask(__name__, static_url_path='/static')
 
-'''games.insert_many([
-    {'title': 'skyrim', 'description': 'description : fun ass game'},
-    {'title': 'yeah', 'description': 'description : fun-ish game'}
-])'''
-
-
 @app.route('/')
 def games_index():
     """Show all games."""
-    game_item = games.find()
-    return render_template('games_index.html', game_item=game_item)
+    game_items = games.find()
+    return render_template('games_index.html', game_items=game_items)
 
-@app.route('/chip/<chips_id>')
-def chips_show(chips_id):
+@app.route('/game/<game_id>')
+def games_show(game_id):
     """Show a single playlist."""
-    chip = chips.find_one({'_id': ObjectId(chips_id)})
-    return render_template('chip_item.html', chip=chip)
+    game = games.find_one({'_id': ObjectId(game_id)})
+    return render_template('game_item.html', game=game)
 
 @app.route('/games/new')
-def chips_new():
-    """Create a new game listing."""
-    return render_template('game_new.html')
+def games_new():
+    """Create a new chip."""
+    return render_template('games_new.html')
+
+@app.route('/games', methods=['POST'])
+def games_submit():
+    """Submit a new playlist."""
+    added_game = {
+        'title': request.form.get('title'),
+        'description': request.form.get('description'),
+        'url': request.form.get('url')
+    }
+    print(added_game)
+    games.insert_one(added_game)
+    return redirect(url_for('games_index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
