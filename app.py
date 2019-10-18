0c7ab1952187187c130b0214287a1f4019945a7f
adgray09 <alexandergray37@yahoo.com>
2019-10-17 17:38:33 -0700

    added more functionality

--- app.py
+++ app.py
@@ -5,12 +5,30 @@ client = MongoClient()
 db = client.Game_feed
 games = db.games
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/static')
+
+'''games.insert_many([
+    {'title': 'skyrim', 'description': 'description : fun ass game'},
+    {'title': 'yeah', 'description': 'description : fun-ish game'}
+])'''
+
 
 @app.route('/')
 def games_index():
     """Show all games."""
-    return render_template('games_index.html', games=games.find())
+    game_item = games.find()
+    return render_template('games_index.html', game_item=game_item)
+
+@app.route('/chip/<chips_id>')
+def chips_show(chips_id):
+    """Show a single playlist."""
+    chip = chips.find_one({'_id': ObjectId(chips_id)})
+    return render_template('chip_item.html', chip=chip)
+
+@app.route('/games/new')
+def chips_new():
+    """Create a new game listing."""
+    return render_template('game_new.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
